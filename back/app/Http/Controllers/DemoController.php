0947b55710b94c62120b3bf99f0f45e0e945c4b5
zekang <zekangzhang@sf-express.com>
2018-10-05 17:06:18 +0800

    demo route

---
+++ back/app/Http/Controllers/DemoController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Desc: This is a demo.
+ * User: zhangzekang
+ * Date: 2018/10/5
+ * Time: 下午12:52
+ */
+namespace app\Http\Controllers;
+
+class DemoController {
+    public function index(){
+        return 'This is a demo.';
+    }
+}
